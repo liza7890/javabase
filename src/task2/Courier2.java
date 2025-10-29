6f355d35d997a5d6f4cb7946d4033f43d5db2dcc
liza7890 <lizamorj@mail.ru>
2025-10-30 01:05:51 +0300

    корект задания

--- src/task2/Courier2.java
+++ src/task2/Courier2.java
@@ -2,19 +2,17 @@ package task2;
 
 public class Courier2 {
     public static void main(String[] args) {
-        int floors = 10; // этажи
-        int flats = 5; // квартиры на этаже
-
-        System.out.println("курьер начинает доставку посылок");
-
-        for (int floor = 1; floor <= floors; floor++) {
-            //перебор всех квартир на текущем этаже
-            for (int flat = 1; flat <= flats; flat++) {
-                System.out.println("Этаж " + floor + ", Квартира " + flat + " - посылка доставлена");
-
+        for (int i = 1; i <= 10; i++) {
+            if (i == 1) {
+                for (int d = 1; d <= 5; d++) {
+                    System.out.println("Этаж N" + i + " квартира N" + d);
+                }
+            } else {
+                for (int d = 5 * i - 4; d <= 5 * i; d++) {
+                    System.out.println("Этаж N" + i + " квартира N" + d);
+                }
             }
         }
-        System.out.println("все посылки доставлены");
 
 
 
